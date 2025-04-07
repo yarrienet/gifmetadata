78d6154ef2c5f36059c26d33a20aedc0ad41b7ac
Harry Stanton <harry@harrystanton.com>
2025-04-07 18:28:53 +0100

    fix: refactored cli.c

--- cli.h
+++ cli.h
@@ -5,16 +5,84 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct cli_args {
-    int all_flag;
-    int verbose_flag;
-    int dev_flag;
-    int help_flag;
-    char *filename;
-    size_t filename_size;
-};
+/**
+ * @struct cli_user_args
+ * @brief Holds the user provided arguments after being parsed.
+ *
+ * Structure holds the parsed command line arguments provided by the user.
+ * Fields are provided for supported flags and an optional filename buffer
+ * with size.
+ *
+ * The presence of a flag or buffer is denoted by a boolean integer or
+ * non-null value.
+ *
+ * @var cli_user_args::all_flag
+ * Marks the presence of the 'all' flag (-a / --all): display all GIF blocks,
+ * not only the comment.
+ *
+ * @var cli_user_args::verbose_flag
+ * Marks the presence of the 'verbose' flag (-v / --verbose): display more data
+ * about the GIF.
+ * 
+ * @var cli_user_args::dev_flag
+ * Marks the presence of the 'dev' flag (-d / --dev): print inner program
+ * workings (intended for developers).
+ *
+ * @var cli_user_args::help_flag
+ * Marks the presence of the 'help' flag (-h / --help): skip all other tasks
+ * and print program help.
+ *
+ * @var cli_user_args::filename
+ * A filename which may be optionally provided within command arguments by
+ * including a loose string. The buffer is dynamically allocated according to
+ * the length of the string, size of the buffer is stored within
+ * @ref cli_ user_args::filename_size. Must be freed using @ref free_user_args.
+ * 
+ * @var cli_user_args::filename_size
+ * Size of the @ref cli_user_args::filename buffer in bytes.
+ *
+ * @note @ref cli_user_args::filename is an allocated buffer and needs to be
+ *       manually deallocated when deallocating the structure itself. Use
+ *       Use helper function @ref cli_free_user_args to deallocate the struct
+ *       and all fields.
+ *     
+ */
+typedef struct cli_user_args {
+    int all_flag; ///< Marks the presence of the 'all' flag
+    int verbose_flag; ///< Marks the presence of the 'verbose' flag
+    int dev_flag; ///< Marks the presence of the 'dev' flag
+    int help_flag; ///< Mark the presence of the 'help' flag
+    char *filename; ///< Optional filename buffer
+    size_t filename_size; ///< Size of filename buffer in bytes
+} cli_user_args;
 
-void free_cli_args(struct cli_args *args);
-struct cli_args *cli_parse(int argc, char **argv);
+/**
+ * @brief Frees the memory allocated for the @ref cli_user_args struct.
+ *
+  Function deallocates the @ref cli_user_args structure and all related fields.
+ *
+ * @param args A pointer to the @ref cli_user_args struct to be freed.
+ */
+void cli_free_user_args(cli_user_args *a);
+/**
+ * @brief Parse command line arguments to produce a structure containing the
+ *        presence of supported flags and arguments.
+ *
+ * Function to parse command line arguments and produce a structure containing
+ * the presence of supported flags and arguments. Only embedded and known flags
+ * and arguments are supported, the presence of unknowns will produce an error.
+ *
+ * @param argc Length of command line arguments, usually provided from the main
+ *             method.
+ *
+ * @param argv List of command line arguments, usually provided from the main
+ *             method.
+ *
+ * @return An allocated @ref cli_user_args structure, must be freed before
+           program exit. Use helper function @ref free_user_args to free
+           structure and allocated fields. On error, null will be returned and 
+           message will be printed in stderr.
+ */
+cli_user_args *cli_parse(int argc, char **argv);
 
 #endif
